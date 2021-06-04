3ba99694a52e6374b68933e707411115780ad264
farrukh90 <farrukhsadykov@gmail.com>
2021-06-04 04:01:53 +0000

    added keys

---
+++ KeyVault/key.tf
@@ -0,0 +1,43 @@
+provider "azurerm" {
+	features {}
+}
+
+
+data "azurerm_client_config" "current" {}
+	resource "azurerm_resource_group" "example" {
+	name = "example-resources"
+	location = "West Europe"
+}
+
+
+resource "azurerm_key_vault" "example" {
+	name = "examplekeyvaultfarrukh"
+	location = azurerm_resource_group.example.location
+	resource_group_name = azurerm_resource_group.example.name
+	enabled_for_disk_encryption = true
+	tenant_id = data.azurerm_client_config.current.tenant_id
+	soft_delete_retention_days = 7
+	purge_protection_enabled = false
+	sku_name = "standard"
+	access_policy {
+		tenant_id = data.azurerm_client_config.current.tenant_id
+		object_id = data.azurerm_client_config.current.object_id
+	key_permissions = [
+		"Get",
+		"List",
+		"Create",
+		"Update",
+		"Delete"
+	]
+	secret_permissions = [
+		"Get",
+		"List",
+		"Set",
+	]
+	storage_permissions = [
+		"Get",
+		"List",
+		"Update",
+		]
+	}
+}
