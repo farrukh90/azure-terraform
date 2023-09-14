f68e4e8ffe434a61a8737eac3de195bf62a24a33
Farrukh Sadykov <farrukhsadykov@gmail.com>
2023-09-13 21:18:26 -0500

    added tags

--- SS/ss.tf
+++ SS/ss.tf
@@ -13,6 +13,10 @@ resource "azurerm_virtual_network" "example" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    environment = "Production"
+  }
 }
 
 resource "azurerm_subnet" "internal" {
